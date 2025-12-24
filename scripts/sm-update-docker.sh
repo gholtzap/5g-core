2960d880c0693ae62b1aa6bb51f495792aa9cdb5
Gavin <gholtzap@asu.edu>
2025-12-24 17:34:01 -0500

    Update submodules

--- scripts/sm-update-docker.sh
+++ scripts/sm-update-docker.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
-docker volume rm $(docker volume ls -q)
-sleep 1
 git submodule update --remote --merge
 git add .
 git commit -m "Update submodules"
 git push origin master
-sleep 5
 docker compose build
-sleep 2
 docker compose up   
