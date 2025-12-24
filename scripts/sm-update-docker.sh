448dd03e9b779bc978402e4a8c9ff48855a486b5
Gavin <gholtzap@asu.edu>
2025-12-24 15:09:58 -0500

    Fix AMF docker configuration

--- scripts/sm-update-docker.sh
+++ scripts/sm-update-docker.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+docker volume rm $(docker volume ls -q)
+sleep 1
 git submodule update --remote --merge
 git add .
 git commit -m "Update submodules"
 git push origin master
+sleep 5
 docker compose build
-docker compose up
+sleep 2
+docker compose up   
