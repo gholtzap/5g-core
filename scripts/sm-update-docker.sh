f39b0154346a771f6af65a93ff67649c8a61fdae
Gavin <gholtzap@asu.edu>
2025-12-29 11:48:07 -0500

    Update submodules

--- scripts/sm-update-docker.sh
+++ scripts/sm-update-docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#docker compose build ueransim-gnb --no-cache
+#docker compose build amf --no-cache
 git submodule update --remote --merge
 git add .
 git commit -m "Update submodules"
 git push origin master
+docker compose build amf --no-cache
 docker compose build
 docker compose up   
