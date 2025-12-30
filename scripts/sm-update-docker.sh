53513b13c24766aae2874abc3746ab172ce523e0
Gavin <gholtzap@asu.edu>
2025-12-30 10:10:29 -0500

    Update submodules

--- scripts/sm-update-docker.sh
+++ scripts/sm-update-docker.sh
@@ -5,5 +5,6 @@ git add .
 git commit -m "Update submodules"
 git push origin master
 docker compose build amf --no-cache
+docker compose build ausf --no-cache
 docker compose build
 docker compose up
